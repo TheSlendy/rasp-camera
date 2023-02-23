aed569ba7c10075febe03e0918de7bd6f76fb63a
TheSlendy <48982621+TheSlendy@users.noreply.github.com>
2023-02-23 13:54:37 +0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,71 +1,71 @@
 import cv2
-from os import listdir, remove, path, mkdir
+from os import listdir, remove, path, mkdir, system
 import re
 from detector import Detector
 from glob import glob
+from client import Sender
 
 
 class Camera:
     def __init__(self, video_source=0):
-        self.cap = cv2.VideoCapture(video_source)
-        self.detector = Detector()
-        self.consecutive = True
-        self.frame_dir = "frames"
+        self._cap = cv2.VideoCapture(video_source)
+        self._detector = Detector()
+        self._frame_dir = "frames"
+        print("Camera Inited")
+        self._sender = Sender()
 
     @staticmethod
-    def atoi(text):
+    def __atoi(text):
         return int(text) if text.isdigit() else text
 
-    def natural_keys(self, text):
-        return [self.atoi(c) for c in re.split(r'(\d+)', text)]
+    def __natural_keys(self, text):
+        return [self.__atoi(c) for c in re.split(r'(\d+)', text)]
 
-    def save_detected_motion(self, img):
+    def __save_detected_motion(self, img):
         try:
-            frame_list = listdir(self.frame_dir)
-        except:
-            mkdir(self.frame_dir)
+            frame_list = listdir(self._frame_dir)
+        except FileNotFoundError:
+            mkdir(self._frame_dir)
+            frame_list = listdir(self._frame_dir)
         if frame_list:
-            frame_list.sort(key=self.natural_keys)
+            frame_list.sort(key=self.__natural_keys)
             frame_number = re.findall(r'\d+', frame_list[-1])[0]
             cv2.imwrite(f"frames/frame#{int(frame_number) + 1}.png", img)
         else:
             cv2.imwrite("frames/frame#1.png", img)
 
-    def make_video(self):
+    def __make_video(self):
         img_array = []
         for filename in glob('frames/*.png'):
             img = cv2.imread(filename)
             height, width, layers = img.shape
             size = (width, height)
             img_array.append(img)
-        try:
-            try:
-                video_list = listdir("motions")
-        	except:
-                mkdir(self.frame_dir)
-            if video_list:
-                video_list.sort(key=self.natural_keys)
-                video_number = int(re.findall(r'\d+', video_list[-1])[0])
-            else:
-                video_number = 0
-            out = cv2.VideoWriter(f'motions/motion#{video_number + 1}.avi', cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
-            for image in img_array:
-                out.write(image)
-            out.release()
-            for f in listdir(self.frame_dir):
-                remove(path.join(self.frame_dir, f))
-        except NameError:
-            return
+        out = cv2.VideoWriter(f'motions/motion.avi', cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+        for image in img_array:
+            out.write(image)
+        out.release()
+        self._sender.send("motion.avi")
+        for f in listdir(self._frame_dir):
+            remove(path.join(self._frame_dir, f))
+        remove(path.join("motions", "motion.avi"))
 
     def run(self):
         while True:
-            _, img = self.cap.read()
-            if self.detector.detect(img):
-                self.save_detected_motion(img)
-                self.consecutive = True
+            _, img = self._cap.read()
+            if self._detector.detect(img):
+                self.__save_detected_motion(img)
+                system('echo 1 | sudo tee /sys/class/leds/led0/brightness')
             else:
-                self.consecutive = False
-            if not self.consecutive:
-                self.make_video()
+                system('echo 0 | sudo tee /sys/class/leds/led0/brightness')
+                try:
+                    frame_list = listdir(self._frame_dir)
+                except FileNotFoundError:
+                    mkdir(self._frame_dir)
+                    frame_list = listdir(self._frame_dir)
+                if frame_list:
+                    print("Making video")
+                    self.__make_video()
             if cv2.waitKey(1) & 0xff == ord('q'):
                 break
+  
