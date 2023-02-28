780d6bb071145b88f88868046dd846d506639b82
TheSlendy <48982621+TheSlendy@users.noreply.github.com>
2023-02-28 17:10:42 +0700

    update detector.py
    provided privacy to detector attributes

--- detector.py
+++ detector.py
@@ -4,20 +4,20 @@ import numpy as np
 
 class Detector:
     def __init__(self):
-        self.prev_frame = None
+        self._prev_frame = None
 
     def detect(self, current_frame):
         frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        if self.prev_frame is None:
-            self.prev_frame = frame
+        if self._prev_frame is None:
+            self._prev_frame = frame
             return
-        difference = cv2.absdiff(self.prev_frame, frame)
-        self.prev_frame = frame
+        difference = cv2.absdiff(self._prev_frame, frame)
+        self._prev_frame = frame
         kernel = np.ones((5, 5))
-        difference = cv2.dilate(difference, kernel, 1)
         thresh = cv2.threshold(difference, 20, 255, cv2.THRESH_BINARY)[1]
-        for i in thresh:
-            if not all(elem == 0 for elem in i):
-                return True
-        return False
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+        areas = [cv2.contourArea(c) for c in contours]
+        if len(areas) < 1:
+            return False
+        return True
